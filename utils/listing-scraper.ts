10648b80bceedf1b613e096b3bdf0a5396c14d31
lbehrndt <leonbehrndt1@gmail.com>
2023-08-14 14:43:47 -0700

    add skeleton and interface

---
+++ utils/listing-scraper.ts
@@ -0,0 +1,11 @@
+import axios, { all } from "axios";
+import cheerio from "cheerio";
+import { IListing } from "../interfaces/IListing";
+
+export default class ListingScraper {
+  constructor() {}
+
+  async getListings() {}
+
+  async addListings() {}
+}
