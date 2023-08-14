bc935e921c3fee6894dd8576643ca8cfde812b04
lbehrndt <leonbehrndt1@gmail.com>
2023-08-14 14:36:12 -0700

    add MessageBot skeleton and message interface

---
+++ utils/message-bot.ts
@@ -0,0 +1,13 @@
+export default class MessageBot {
+  constructor() {}
+
+  async login() {}
+
+  async getMessageTemplate() {}
+
+  async getMessageData() {}
+
+  async sendMessage() {}
+
+  async contactListings() {}
+}
